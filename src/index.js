35bc5930ea418c8d762f7d269b12d357b6a1c0d9
IhorPozhematkin <132217496+IhorPozhematkin@users.noreply.github.com>
2023-10-08 21:35:18 +0300

    v.0.2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import axios from "axios";
-import Notiflix from 'notiflix';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+// import axios from "axios";
+// import Notiflix from 'notiflix';
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
 
 // https://pixabay.com/api/docs/#api_key
 // 39917571-28ef3a52fe9a456424b2da7ea
